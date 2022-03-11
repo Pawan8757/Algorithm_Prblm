f3034054f127ac8c1b4d52bcc9b7ed704d4e32cb
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-11 07:13:05 +0530

    adding Intro file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arithmatic
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Here is the program of algorithm logic problem");
+        }
+    }
+}
