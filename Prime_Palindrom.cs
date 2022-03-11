79a3f89ecf146140d675b9f056c3d3ab75be9162
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-11 09:44:30 +0530

    adding Prime_Palindrome file

---
+++ Prime_Palindrom.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Prime_Palindrome
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int flag = 0;
+            for (int n = 0; n <= 1000; n++)
+            {
+                flag = 0;
+                for (int i = 2; i <= n / 2; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        flag = 1;
+                    }
+                }
+
+                if (flag == 0)
+                {
+                    Console.WriteLine("prime" + n);
+                }
+
+                if (n % 11 == 0)
+                {
+                    Console.WriteLine("Palindrome" + n);
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ Prime_Range.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNoRange;
+
+public class PrimeFactor
+{
+    public static void Main(string[] args)
+    {
+        int flag = 0;
+        for (int n = 0; n <= 1000; n++)
+        {
+            flag = 0;
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    flag = 1;
+                }
+            }
+
+            if (flag == 0)
+            {
+                Console.WriteLine("Prime = " + n);
+            }
+        }
+    }
+}
+
