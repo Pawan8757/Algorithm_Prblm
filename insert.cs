18a4b440ad08b3ada995fee61a98834310fa530e
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-11 09:41:24 +0530

    adding insert program

---
+++ insert.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Insertsort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = new int[5] { 23, 56, 88, 99, 11 };
+            int n = 6, i, j, val, flag;
+
+            for (i = 0; i <= n; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+            for (i = 1; i <= n; i++)
+            {
+                val = a[i];
+            }
+
+            flag = 0;
+
+            for (j = i - 1; j >= 0 && flag != 1;)
+            {
+                if (val < a[j])
+                {
+                    a[j + 1] = a[j];
+                    j--;
+                    a[j + 1] = val;
+                }
+
+
+                else flag = 1;
+            }
+
+            Console.WriteLine("sorted");
+        }
+    }
+}
+
+
