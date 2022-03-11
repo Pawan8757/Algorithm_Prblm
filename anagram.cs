9fc3a40f13dc1bc9ebdd6130337e24a93f8b8618
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-11 09:38:16 +0530

    adding anagram file

---
+++ anagram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Anagram
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "heater";
+            string str2 = "reheat";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str1.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("anagram");
+            }
+
+            else
+            {
+                Console.WriteLine("Not anagram");
+            }
+        }
+    }
+}
